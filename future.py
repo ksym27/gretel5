99ded56a8fd161dab0276499f5e62c3feacaf167
ksym29 <t.kashiyama@osaka-ue.ac.jp>
2022-07-15 19:11:33 +0900

    init

---
+++ future.py
@@ -0,0 +1,8 @@
+class Future:
+
+    def __init__(self, observations, start, target, prediction):
+        self.observations = observations
+        self.start = start
+        self.target = target
+        self.prediction = prediction
+
